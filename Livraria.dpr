8e265b9910a53fc4e66d5a900d5c915ef861abb6
JuliaHFerreira <juliahelena317@gmail.com>
2024-08-13 17:25:53 -0300

    Criando Menu e separando telas

--- Livraria.dpr
+++ Livraria.dpr
@@ -2,7 +2,8 @@ program Livraria;
 
 uses
   Vcl.Forms,
-  UnitPrincipal in 'UnitPrincipal.pas' {Form1};
+  UnitPrincipal in 'UnitPrincipal.pas' {Form1},
+  CadastroLivro in 'CadastroLivro.pas' {Form2};
 
 {$R *.res}
 
@@ -10,5 +11,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
--- UnitPrincipal.pas
+++ UnitPrincipal.pas
@@ -4,29 +4,30 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.ComCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.ComCtrls,
+  Vcl.Menus;
 
 type
   TForm1 = class(TForm)
-    Label1: TLabel;
-    Label2: TLabel;
-    Label3: TLabel;
-    Label4: TLabel;
-    CheckBox4: TCheckBox;
-    Label6: TLabel;
-    CheckBox1: TCheckBox;
-    CheckBox2: TCheckBox;
-    CheckBox3: TCheckBox;
-    Button1: TButton;
-    Button2: TButton;
-    Button3: TButton;
-    Edit1: TEdit;
-    Edit2: TEdit;
-    ComboBox2: TComboBox;
-    Memo1: TMemo;
-    GroupBox1: TGroupBox;
-    Panel1: TPanel;
-    RadioGroup1: TRadioGroup;
+    MainMenu1: TMainMenu;
+    Sistema1: TMenuItem;
+    Usurios1: TMenuItem;
+    Configuraes1: TMenuItem;
+    N1: TMenuItem;
+    Sair1: TMenuItem;
+    Cadastro1: TMenuItem;
+    Clientes1: TMenuItem;
+    Funcionarios1: TMenuItem;
+    Livros1: TMenuItem;
+    N2: TMenuItem;
+    Consultas1: TMenuItem;
+    Clientes2: TMenuItem;
+    Clientes3: TMenuItem;
+    Autores1: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    CriarnovoUsurio1: TMenuItem;
+    rocardeUsurio1: TMenuItem;
   private
     { Private declarations }
   public
